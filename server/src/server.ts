66573e7c24c0afb3a0164cabaf5df47547b2000a
bruno takashi <bt.tengan@gmail.com>
2020-09-07 20:57:22 -0300

    Back-end project initial setup
    Some concepts here:
    - Routes and Resources
    - HTTP methods
    - Parameters

---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+
+// introduz o pacote para compreender json.
+app.use(express.json());
+
+// COMANDOS HTTP
+// GET: Buscar ou listar uma informação.
+// POST: Criar alguma nova informação.
+// PUT: Atualizar uma informação existente.
+// DELETE: Deleta uma informação existente.
+
+// PARAMETROS
+// Corpo (request body): Dados para criação ou atualização de um registro. request.body
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar. request.params
+// Query Params: Paginação, filtros, ordenação. request.query
+
+app.get('/users', (request, response) => {
+    console.log(request.query);
+    
+    const users = [
+        {name: 'bruno', age: 26},
+        {name: 'he', age: 15},
+    ];
+    
+    return response.json(users);
+});
+
+// porta padrão é número 80. Todos os sites tem no final mas é oculto.
+// ex: www.minhaapi.com.br:3333
+// localhost:3333
+app.listen(3333);
