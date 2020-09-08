3265ea86f7c43b48e3b70dd60208b795d248ae00
bruno takashi <bt.tengan@gmail.com>
2020-09-07 22:41:26 -0300

    Route to register classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 // introduz o pacote para compreender json.
 app.use(express.json());
-
-// COMANDOS HTTP
-// GET: Buscar ou listar uma informação.
-// POST: Criar alguma nova informação.
-// PUT: Atualizar uma informação existente.
-// DELETE: Deleta uma informação existente.
-
-// PARAMETROS
-// Corpo (request body): Dados para criação ou atualização de um registro. request.body
-// Route Params: Identificar qual recurso eu quero atualizar ou deletar. request.params
-// Query Params: Paginação, filtros, ordenação. request.query
-
-app.get('/', (request, response) => {
-    return response.json({message: "hello world"});
-});
+app.use(routes);
 
 // porta padrão é número 80. Todos os sites tem no final mas é oculto.
 // ex: www.minhaapi.com.br:3333
